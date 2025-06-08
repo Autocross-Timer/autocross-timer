72d8f062056fa0a62753d1fb2040dcfffb2fea41
Jared Kelly <jaredksemail@gmail.com>
2025-06-08 01:16:26 -0400

    adding class filter

--- frontend/src/views/LeaderboardView.vue
+++ frontend/src/views/LeaderboardView.vue
@@ -9,6 +9,7 @@
   const route = useRoute();
   const eventId = route.params.eventId;
   const classes = ref(null)
+  const sortClass = route.params.sortClass;
   watch(() => route.params.sortClass, (newSortClass) => {
     updateActiveButton(newSortClass);
   });
@@ -33,7 +34,7 @@
     <RouterLink :to="'/event/' + eventId + '/leaderboard/pax'" class="link-container">PAX</RouterLink>
     <RouterLink :to="'/event/' + eventId + '/leaderboard/raw'" class="link-container">Raw</RouterLink>
     <RouterLink v-for="carClass in classes" :to="'/event/' + eventId + '/leaderboard/' + carClass" class="link-container">{{ carClass }}</RouterLink>
-    <GetRun :sortType="sortType" :leaderboard="true" :eventId="eventId"/>
+    <GetRun :sortType="sortType" :leaderboard="false" :filterClass="sortClass" :eventId="eventId"/>
 </template>
 
 <style scoped>
