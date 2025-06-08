fcc88e2aad2673b0f2e25729a00d14dad6d64b02
Jared Kelly <jaredksemail@gmail.com>
2025-06-08 01:10:56 -0400

    fixing leaderboard links

--- frontend/src/views/LeaderboardView.vue
+++ frontend/src/views/LeaderboardView.vue
@@ -32,7 +32,7 @@
     <NavLinks />
     <RouterLink :to="'/event/' + eventId + '/leaderboard/pax'" class="link-container">PAX</RouterLink>
     <RouterLink :to="'/event/' + eventId + '/leaderboard/raw'" class="link-container">Raw</RouterLink>
-    <RouterLink v-for="carClass in classes" :to="'/event/' + eventId + '/leaderboard/' + carClass.carClass" class="link-container">{{ carClass.carClass }}</RouterLink>
+    <RouterLink v-for="carClass in classes" :to="'/event/' + eventId + '/leaderboard/' + carClass" class="link-container">{{ carClass }}</RouterLink>
     <GetRun :sortType="sortType" :leaderboard="true" :eventId="eventId"/>
 </template>
 
