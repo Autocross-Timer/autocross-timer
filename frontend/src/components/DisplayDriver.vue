ee84550dc357e3268a594d88cc43d4f0657a075d
Andrew Reynolds <reynolds.ottawa@gmail.com>
2025-07-13 17:42:02 -0400

    Adding filtering to only show best times. Add labels to clear up raw vs pax

--- frontend/src/components/DisplayDriver.vue
+++ frontend/src/components/DisplayDriver.vue
@@ -6,14 +6,10 @@
     const driversName = ref(runs[0].driverName);
     const carNumber = ref(runs[0].carNumber);
     const carClass = ref(runs[0].carClass);
-    const bestRawTime = ref(Math.min(...runs.map(run => run.rawTime)));
-    const bestPaxTime = ref(Math.min(...runs.map(run => run.paxTime)));
 </script>
 
 <template>
     <p>Driver: {{ driversName }}</p>
     <p>Car Number: {{ carNumber }}</p>
     <p>Car Class: {{ carClass }}</p>
-    <p>Best Raw Time: {{ bestRawTime }}</p>
-    <p>Best Pax Time: {{ bestPaxTime }}</p>
 </template>
